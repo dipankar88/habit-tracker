3a13a1fb732fb3b041461cb8c9f8d69bf21e4adb
Dipankar Ghosh <dgkgecece@gmail.com>
2026-05-24 23:12:52 +0530

    introduce useState to enter name value for habit

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export function HabitForm() {
+    const [name, setName] = useState("");
+    
     return <form className="flex gap-2">
-        <input 
+        <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="flex-1 rounded-lg bg-zinc-800 py-2 px-4 outline-none focus-visible:ring-2 focus-visible:ring-violet-500" 
             placeholder="New habit..."
         />
